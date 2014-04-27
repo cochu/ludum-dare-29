4f87f4bb54cfb6ccc8f3a5b5451709f9353235b9
Alexandre Cochu <alex.cochu@gmail.com>
2014-04-27 14:35:09 +0200

    rainbow

--- js/game.js
+++ js/game.js
@@ -26,6 +26,8 @@ raster_powerup.visible = false;
 raster_licorne.visible = false;
 raster_sub.visible = false;
 
+raster_oiseau.name = "mouette";
+
 var torpedos = new Array();
 var targets = new Array();
 var explosions = new Array();
@@ -41,7 +43,27 @@ var unicornMode = 0;
 
 var shotOrder = false;
 
-var sound = new Howl({  urls: ['sound/sounds.wav'],  sprite: {    blast: [0, 800]  }});// shoot the laser!sound.play('laser');
+var nbrExplosion=0;
+
+var sound = new Howl({  urls: ['sound/sounds.mp3', 'sound/sounds.ogg'],  sprite: {    
+									start: [39, 1262-39],  
+									bloub1: [1384, 1833-1384], 
+									bloub2: [1840, 2572-1840],  
+									bloub3: [2620, 3176-2620], 
+									torpedo1: [3234, 3456-3234],  
+									torpedo2: [3533, 3795-3234], 
+									torpedo3: [3795, 4147-3795],  
+									torpedo4: [3795, 4337-3795], 
+									explo1: [4375, 5299-4375],  
+									explo2: [5415, 6030-5415], 
+									explo3: [6113, 6791-6113],  
+									explo4: [6786, 7128-6786], 
+									unicorn: [7176, 9569-7176],  
+									mouette1: [9788, 10416-9788], 
+									mouette2: [10482, 11232-10482],  
+									mouette3: [11307, 12162-11307],
+									sonar: [12256, 975] 
+									}});// shoot the laser!sound.play('laser');
 
 raster_sub.position = view.center - new Point(300,0);
 
@@ -106,6 +128,7 @@ var level = new PointText({
 
 	gameText.visible = true;
 	gameText.content = "Cliquez pour jouer !"
+	sound.play('start');
 
 
 function init() {
@@ -124,6 +147,7 @@ function init() {
 		unicornMode = 0;
 		
 		shotOrder = false;
+		nbrExplosion = 0;
 	
 		for (var i = 0; i < torpedos.length; i++) {
 			torpedos[i].visible = false;
@@ -158,6 +182,15 @@ function onMouseUp(event) {
 	shotOrder = false;
 }
 
+function alea(n) {
+
+	var t = Math.random();
+	t = t*n+1;
+	return Math.floor(t);
+	
+}
+
+
 function onMouseDown(event) {
 	if (status ==1) { 
 		shotOrder = true;
@@ -173,7 +206,9 @@ function hitTest() {
 	if (raster_powerup.visible == true) {
 		var hit = raster_powerup.hitTest(raster_sub.position);
 		if (hit != null) {
-			unicornMode = 10000;
+			sound.play('unicorn');
+
+			unicornMode = 11500;
 			raster_powerup.visible = false;
 			raster_licorne.position = raster_sub;
 			raster_sub.visible = false;
@@ -193,7 +228,16 @@ function hitTest() {
 					explosion.visible = true;
 					explosion.rotate((Math.random()-0.5)*100);
 					explosions.push( { sprite:explosion, life:500 } ); 
-					sound.play('blast');
+					if (targets[i].type == 'oiseau'){
+						var n = alea(3);
+						var s = 'mouette' + n;
+						sound.play(s);
+
+					} else {
+						var n = alea(4);
+						var s = 'explo' + n;
+						sound.play(s);
+					}
 					Score += 100;
 					targets[i].sprite.visible = false;
 					torpedos[j].visible = false;
@@ -209,6 +253,12 @@ function hitTest() {
 		if (targets[i] != null && unicornMode == 0) {
 			var hit = targets[i].sprite.hitTest(raster_sub.position);
 			if (hit != null) {
+				var explosion = raster_explo1.clone();	
+				explosion.position = raster_sub.position;
+				explosion.visible = true;
+				explosion.rotate((Math.random()-0.5)*100);
+				explosions.push( { sprite:explosion, life:300 } ); 
+				sound.play('explo1');
 				targets[i].sprite.visible = false;
 				raster_sub.visible = false;
 				targets.splice(i,1);
@@ -234,24 +284,26 @@ function powerup() {
 }
 
 function newTarget() {
-	var difficulty = (Timer*0.005)-(Level*0.01);
+	var difficulty = (Timer*0.001)-(Level*0.01);
 	if (difficulty >= 0.99) { 
 		difficulty = 0.99;
 	}
 	if (Math.random() > (0.99-difficulty)) {
 		var newspeed = (200 + Timer*0.001 + Level*10)*(Math.random()+0.5);
-		console.log(newspeed);
 		if (Math.random() > 0.50) {
 			var target = raster_poisson.clone();
+			var newtype = 'poisson';
 		} else {
 			var target = raster_oiseau.clone();
+			var newtype = 'oiseau';
 		}
 		var pos = new Point( (view.size.width+200), (100+Math.random()*(view.size.height-200)));
 		target.position = pos;
 		target.visible = true;
 		targets.push({ 
 										sprite:target, 
-										speed:newspeed 
+										speed:newspeed,
+										type:newtype
 									});
 	}
 }
@@ -260,8 +312,41 @@ function leveling() {
 
 	if (Score > 1000*Level) {
 		Level += 1;
+		sound.play('sonar');
 	}
 
+}
+
+var rain = new Group();
+var rainLen = 30;
+var rainWidth = 10;
+var rainStep = 10;
+var colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple'];
+for (var c=0; c<7; c++) {
+	var path = new Path();
+	for (var i=0; i<rainLen; i++) {
+		path.strokeWidth = 10;
+		path.strokeColor = colors[c];
+		path.add( new Point(view.center.x-rainStep*i, view.center.y + (c-3)*rainWidth) );
+	}
+	rain.addChild(path);
+}
+rain.visible = false;
+
+function rainbow(dt) {
+	if (rain.visible == false) {
+		rain.visible = true;
+	}
+	for (var c=0; c<rain.children.length; c++) {
+		path = rain.children[c];
+		for (var i=path.segments.length - 1; i > 0; i--  ) {
+		
+			path.segments[i].point = path.segments[i-1].point + new Point(-10,0);
+		
+		}
+
+		path.firstSegment.point = raster_sub.position + new Point(-80-(c-3)*8, (c-3)*rainWidth);
+	}
 }
 
 function onFrame(event) {
@@ -270,11 +355,14 @@ function onFrame(event) {
 	lastShot -= dt*1000;
 	
 	if (status == 1) {
+		
 		if (unicornMode > 0) {
+			rainbow();
 			unicornMode -= dt*1000;
 			raster_licorne.position = raster_sub.position;
 			//raster_licorne.fillColor.hue += 1;
 			if (unicornMode <= 0) {
+				rain.visible = false;
 				unicornMode = 0;
 				raster_sub.visible = true;
 				raster_licorne.visible = false;
@@ -289,13 +377,16 @@ function onFrame(event) {
 		
 		raster_sub.position += (mousePos - raster_sub.position)*0.05;
 
-		if (lastShot <= 0 && shotOrder == true) {
-			if (unicornMode > 0) {
+		if (lastShot <= 0 && shotOrder == true && unicornMode < 10000) {
+			if (unicornMode > 0) { 
 				lastShot = 1000/(10+Level*0.01);
 			}
 			else {
 				lastShot = 1000/(1+Level*0.01);
 			}
+			var n = alea(4);
+			var s = 'torpedo' + n; 
+			sound.play(s);
 			var torpedo = raster_torpedo.clone();
 			torpedo.position = raster_sub.position + new Point(150,10);
 			torpedo.visible = true;
@@ -324,6 +415,9 @@ function onFrame(event) {
 		for (var i = 0; i < torpedos.length; i++) {
 			torpedos[i].position += new Point(300*dt,0);
 			if (torpedos[i].position.x > view.size.width) {
+				var n = alea(3);
+				var s = 'bloub' + n; 
+				sound.play(s);
 				torpedos[i].visible = false;
 				torpedos.splice(i,1);
 				i--;
@@ -349,5 +443,21 @@ function onFrame(event) {
 		hitTest();
 		leveling();
 		powerup();
+	} else if (status == 2 && nbrExplosion<10) {
+		for (var i = 0; i < explosions.length; i++) {
+			explosions[i].life -= dt*1000;
+			if (explosions[i].life <= 0) {
+				nbrExplosion++;
+				var explosion = raster_explo1.clone();	
+				explosion.position = explosions[i].position + new Point((Math.random()-0.5)*10, (Math.random()-0.5)*10);
+				explosion.visible = true;
+				explosion.rotate((Math.random()-0.5)*100);
+				sound.play('explo1');
+				explosions[i].sprite.visible = false;
+				explosions.splice(i,1);
+				i--;
+				explosions.push( { sprite:explosion, life:(300-(nbrExplosion*25)) } ); 
+			}
+		}
 	}
 }
